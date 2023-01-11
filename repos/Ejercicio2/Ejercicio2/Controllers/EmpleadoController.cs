a7bdbac715d1ff3cd8985d7e271c5e46b5a9d226
Martinez <martin.martinez@softwareone.com>
2023-01-11 11:04:43 +0100

    Ejercicio 2 .Net. Guardar y listar

---
+++ repos/Ejercicio2/Ejercicio2/Controllers/EmpleadoController.cs
@@ -0,0 +1,30 @@
+﻿using Ejercicio2.Config;
+using Ejercicio2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ejercicio2.Controllers
+{
+    public class EmpleadoController : Controller //la herencia en c# es con los ':'
+    {
+        private readonly DBEmpleadoContext context;
+        public EmpleadoController(DBEmpleadoContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet("list")]
+        public async Task<ActionResult<List<Empleado>>> Get()
+        {
+            return await this.context.Empleados.ToListAsync();
+        }
+
+        [HttpPost("save")]
+        public async Task<ActionResult> Post(Empleado empleado) {
+            this.context.Add(empleado);
+            await this.context.SaveChangesAsync();
+
+            return Ok();
+                }
+    }
+}
